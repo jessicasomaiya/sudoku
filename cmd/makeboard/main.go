d47c41f1f17758bd22bf5da1ce2b27760d42fd2c
jessica somaiya <jessica.somaiya@atos.net>
2020-07-25 15:31:17 +0100

    full sudoku board maker

--- cmd/makeboard/main.go
+++ cmd/makeboard/main.go
@@ -1,12 +1,23 @@
 package main
 
 import (
+	"log"
+	"os"
+
 	"github.com/jessicasomaiya/sudoku/pkg/solution"
 )
 
 func main() {
+	b, err := os.Create("boards")
+	if err != nil {
+		log.Fatal("output cannot be created")
+	}
 	s := solution.InitSudoku(9)
-	s.FillBoard()
 
-	s.PrintNinePretty()
+	// root := solution.CreateRoot(2)
+	// mcts := solution.NewMCTS(root)
+	// mcts.Gamble(1000)
+	s.FillWholeBoard(5000, b)
+
+	// s.PrintNinePretty(b)
 }
